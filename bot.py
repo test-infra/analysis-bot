cd9ff3b5c39699df6b25c4b6a73b77ab28eec0cd
trinhvv <vanvuong.trinh@gmail.com>
2018-08-25 23:09:05 +0200

    Commit time: 2018-08-25 23:09:05

--- bot.py
+++ bot.py
@@ -14,60 +14,60 @@ SECRET_KEY=os.environ['SECRET_KEY']
 API_KEY=os.environ['API_KEY']
 client=Client(API_KEY,SECRET_KEY)
 
-DB_NAME=os.environ['DB_NAME']
-DB_USERNAME=os.environ['DB_USERNAME']
-DB_HOST=os.environ['DB_HOST']
-DB_PASSWORD=os.environ['DB_PASSWORD']
-DB_URL="dbname='"+DB_NAME+"' user='"+DB_USERNAME+"' host='"+DB_HOST+"' password='"+DB_PASSWORD+"'"
-
-conn=psycopg2.connect(DB_URL)
-cur=conn.cursor()
-cur.execute("SELECT chat_id FROM users")
-users=cur.fetchall()
-id_list=[chat_id[0] for chat_id in users]
-cur.close()
-conn.close()
+#DB_NAME=os.environ['DB_NAME']
+#DB_USERNAME=os.environ['DB_USERNAME']
+#DB_HOST=os.environ['DB_HOST']
+#DB_PASSWORD=os.environ['DB_PASSWORD']
+#DB_URL="dbname='"+DB_NAME+"' user='"+DB_USERNAME+"' host='"+DB_HOST+"' password='"+DB_PASSWORD+"'"
+#
+#conn=psycopg2.connect(DB_URL)
+#cur=conn.cursor()
+#cur.execute("SELECT chat_id FROM users")
+#users=cur.fetchall()
+#id_list=[chat_id[0] for chat_id in users]
+#cur.close()
+#conn.close()
 
 ADMIN_ID=os.environ['ADMIN_ID']
 ADMIN_USERNAME=os.environ['ADMIN_USERNAME']
 
-def send_msg(bot,update):
-    if str(update.message.from_user.username)==ADMIN_USERNAME:
-        msg=update.message.text
-        if msg[0:10]=='/send_msg ':
-            msg=msg[10:]
-            for id_item in id_list:
-                bot.send_message(chat_id=id_item,text=msg,parse_mode=ParseMode.MARKDOWN)
-                
-def nofity(bot,job):
-    alarm,msg=btclib.btc_alarm()
-    if alarm:
-        for id_item in id_list:
-#            bot.send_message(chat_id=job.context["chat_id"],text=msg,parse_mode=ParseMode.MARKDOWN)
-            bot.send_message(chat_id=id_item,text=msg,parse_mode=ParseMode.MARKDOWN)
-
-def btc(bot,update,job_queue,chat_data):
-    if str(update.message.from_user.username)==ADMIN_USERNAME:
-        bot.send_chat_action(chat_id=update.message.chat_id,action=telegram.ChatAction.TYPING)
-        time_period=250
-        if 'job' not in chat_data:
-#            job=job_queue.run_repeating(nofity,interval=time_period,first=0,context={"chat_id": update.message.chat_id})
-            job=job_queue.run_repeating(nofity,interval=time_period,first=0)
-            chat_data['job']=job
-            update.message.reply_text('Bitfinex BTC monitoring set.')
-        else:
-            update.message.reply_text('Please unset current Bitfinex BTC monitoring.')
-        
-def u(bot,update,chat_data):
-    if str(update.message.from_user.username)==ADMIN_USERNAME:
-        bot.send_chat_action(chat_id=update.message.chat_id,action=telegram.ChatAction.TYPING)
-        if 'job' not in chat_data:
-            update.message.reply_text('You have no active monitoring.')
-            return
-        job=chat_data['job']
-        job.schedule_removal()
-        del chat_data['job']
-        update.message.reply_text('Bitfinex BTC monitoring unset.')
+#def send_msg(bot,update):
+#    if str(update.message.from_user.username)==ADMIN_USERNAME:
+#        msg=update.message.text
+#        if msg[0:10]=='/send_msg ':
+#            msg=msg[10:]
+#            for id_item in id_list:
+#                bot.send_message(chat_id=id_item,text=msg,parse_mode=ParseMode.MARKDOWN)
+#                
+#def nofity(bot,job):
+#    alarm,msg=btclib.btc_alarm()
+#    if alarm:
+#        for id_item in id_list:
+##            bot.send_message(chat_id=job.context["chat_id"],text=msg,parse_mode=ParseMode.MARKDOWN)
+#            bot.send_message(chat_id=id_item,text=msg,parse_mode=ParseMode.MARKDOWN)
+#
+#def btc(bot,update,job_queue,chat_data):
+#    if str(update.message.from_user.username)==ADMIN_USERNAME:
+#        bot.send_chat_action(chat_id=update.message.chat_id,action=telegram.ChatAction.TYPING)
+#        time_period=250
+#        if 'job' not in chat_data:
+##            job=job_queue.run_repeating(nofity,interval=time_period,first=0,context={"chat_id": update.message.chat_id})
+#            job=job_queue.run_repeating(nofity,interval=time_period,first=0)
+#            chat_data['job']=job
+#            update.message.reply_text('Bitfinex BTC monitoring set.')
+#        else:
+#            update.message.reply_text('Please unset current Bitfinex BTC monitoring.')
+#        
+#def u(bot,update,chat_data):
+#    if str(update.message.from_user.username)==ADMIN_USERNAME:
+#        bot.send_chat_action(chat_id=update.message.chat_id,action=telegram.ChatAction.TYPING)
+#        if 'job' not in chat_data:
+#            update.message.reply_text('You have no active monitoring.')
+#            return
+#        job=chat_data['job']
+#        job.schedule_removal()
+#        del chat_data['job']
+#        update.message.reply_text('Bitfinex BTC monitoring unset.')
                 
 def t(bot,update,args):
     bot.send_chat_action(chat_id=update.message.chat_id,action=telegram.ChatAction.TYPING)
@@ -129,22 +129,22 @@ def h(bot,update):
         bot.sendMessage(ADMIN_ID,'chat_id: '+str(update.message.chat_id)+' username: @'+str(update.message.from_user.username)+' cmd: h')
                 
 def manual(bot,update):
-    global id_list
-    if update.message.chat_id not in id_list:
-        id_list.append(update.message.chat_id)
-        conn=psycopg2.connect(DB_URL)
-        cur=conn.cursor()
-        cur.execute("SELECT chat_id FROM users")
-        users=cur.fetchall()
-        id_list_old=[chat_id[0] for chat_id in users]
-        new_id_list=list(set(id_list)-set(id_list_old))
-        for new_id in new_id_list:
-            db_cmd="INSERT INTO users (chat_id) VALUES ('"+str(new_id)+"')"
-            cur.execute(db_cmd)
-        conn.commit()
-        cur.close()
-        conn.close()
-        bot.send_message(chat_id=ADMIN_ID,text="ID list: "+str(id_list))
+#    global id_list
+#    if update.message.chat_id not in id_list:
+#        id_list.append(update.message.chat_id)
+#        conn=psycopg2.connect(DB_URL)
+#        cur=conn.cursor()
+#        cur.execute("SELECT chat_id FROM users")
+#        users=cur.fetchall()
+#        id_list_old=[chat_id[0] for chat_id in users]
+#        new_id_list=list(set(id_list)-set(id_list_old))
+#        for new_id in new_id_list:
+#            db_cmd="INSERT INTO users (chat_id) VALUES ('"+str(new_id)+"')"
+#            cur.execute(db_cmd)
+#        conn.commit()
+#        cur.close()
+#        conn.close()
+#        bot.send_message(chat_id=ADMIN_ID,text="ID list: "+str(id_list))
     bot.send_message(chat_id=update.message.chat_id,text="@trading\_analysis\_bot is a Telegram chatbot for data-driven analytics of crypto-market with technical indicators, social sentiment, developer activities and metrics related to crossed-network on-chain transactions. The aim is to assist traders on Binance exchange.\n*Features*\n- Technical indicators: RSI, MA, BB, etc\n- Order flow: Buy vs sell, Volume profile, Limit orderbook\n- Cryptoasset indexes: Bletchley, Bitwise, CRIX\n- Cryptoasset metrics: TX vol, NVT ratio, num active addresses, num transactions\n- Social sentiment and developer activities: Twitter, Reddit, Facebook, GitHub\n- Trading sessions: New York, London, Tokyo, Sydney\n- Customized notifications: Bitfinex BTCUSD abnormal volume\n*Commands*\n- /a <coin-name-1> <market-name-2> <coin-name-3> <number-of-recent-trades> - Transactions volume versus price statistics. The argument <number-of-recent-trades> can be 500, 1000, 1500, 2000, 2500, 5000, 7500, 10000, 12500, 15000, 17500, 20000, 25000, 30000, 35000, 40000, 45000, 50000 (can be omitted). Examples: /a qtumusdt hot bcn or /a hot npxs btcusdt 20000.\n- /t <coin-name-1> <market-2> <coin-name-3> <chart-flag> - Recent trades summary. If <chart-flag>=1, the volume vs price plot will be provided (can be omitted). Examples: /t hot bat mco 1 or /t npxs btcusdt.\n- /i <coin-name> - Coin information. Examples: /i hot or /i npxs.\n- /m - Market indexes.\n- /h - Trading sesions.\n*Supports*\nIf you don't have a crypto-trading account yet please use the these links to register to [Binance](https://www.binance.com/?ref=13339920) or [Huobi](https://www.huobi.br.com/en-us/topic/invited/?invite_code=x93k3).\nTipjar:\n- BTC: 1DrEMhMP5rAytKyKXRzc6szTcUX8bZzZgq\n- ETH: 0x3915D216f9Fc6ec08f956555e84385513dE5f214\n- LTC: LX8GJkGTZFmAA7puCyVp48333iQdCN6vca\n*Contact*\n@tjeuly",parse_mode=ParseMode.MARKDOWN,disable_web_page_preview=True)
     if str(update.message.from_user.username)!=ADMIN_USERNAME:
         bot.sendMessage(ADMIN_ID,'chat_id: '+str(update.message.chat_id)+' username: @'+str(update.message.from_user.username))
@@ -159,10 +159,10 @@ def main():
     dp.add_handler(CommandHandler("i",i,pass_args=True))
     dp.add_handler(CommandHandler("m",m))
     dp.add_handler(CommandHandler("h",h))
-    dp.add_handler(CommandHandler("btc",btc,pass_job_queue=True,pass_chat_data=True))
-    dp.add_handler(CommandHandler("u",u,pass_chat_data=True))
-    dp.add_handler(MessageHandler(Filters.text,send_msg))
-    dp.add_handler(MessageHandler(Filters.command,send_msg))
+#    dp.add_handler(CommandHandler("btc",btc,pass_job_queue=True,pass_chat_data=True))
+#    dp.add_handler(CommandHandler("u",u,pass_chat_data=True))
+#    dp.add_handler(MessageHandler(Filters.text,send_msg))
+#    dp.add_handler(MessageHandler(Filters.command,send_msg))
     updater.start_polling()
     updater.idle()
 
