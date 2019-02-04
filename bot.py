ce08616a2ffbc06471e881ccf9cecf7023726a0f
trinhvv <vanvuong.trinh@gmail.com>
2019-02-04 12:11:24 +0100

    make it better

--- bot.py
+++ bot.py
@@ -7,14 +7,13 @@ import numpy as np
 import pandas as pd
 from binance_trading_bot import utilities, analysis, visual
 
-MANUAL_TEXT = """@trading\_analysis\_bot
- is a Telegram chatbot for data-driven analytics of crypto-market on Binance.
+MANUAL_TEXT = """@trading\_analysis\_bot is a Telegram chatbot for data-driven analytics of crypto-market on Binance.
  *Features*
- - Technical indicators: MA, BB, Ichimoku, VRVP ...
- - Order flow: Buy vs sell volume, Trades, Limit order book
- - Indexes: Bletchley, Bitwise, CRIX
- - Metrics: TX vol, NVT ratio, num active addresses, num transactions
- - Sentiment and development: Twitter, Reddit, Facebook, GitHub
+ - Technical indicators: MA, BB, Ichimoku, VRVP 
+ - Orderflow: Buy/sell volume, Limit order book
+ - Market indexes: Bletchley, Bitwise, CRIX 
+ - On-chain metrics: NVTS, MVRV Z-Score
+ - Sentiment and development: Twitter, Reddit, GitHub
  - Trading sessions: New York, London, Tokyo, Sydney
  - Customized notifications
  *Commands*
@@ -25,9 +24,7 @@ MANUAL_TEXT = """@trading\_analysis\_bot
  - /m - Market indexes.
  - /h - Trading sesions.
  *Supports*
- If you don't have an account yet please use the these links to register to
- [Binance](https://www.binance.com/?ref=13339920) or
- [Huobi](https://www.huobi.br.com/en-us/topic/invited/?invite_code=x93k3).
+ If you don't have an account yet please use the these links to register to [Binance](https://www.binance.com/?ref=13339920) or [Huobi](https://www.huobi.br.com/en-us/topic/invited/?invite_code=x93k3).
  Tipjar:
  - BTC: 1DrEMhMP5rAytKyKXRzc6szTcUX8bZzZgq
  - ETH: 0x3915D216f9Fc6ec08f956555e84385513dE5f214
