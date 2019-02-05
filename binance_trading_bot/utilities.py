dcd7daa2f290d5ebe4676f2014bab6eeb06811ef
trinhvv <vanvuong.trinh@gmail.com>
2019-02-05 18:44:00 +0100

    make it better

--- binance_trading_bot/utilities.py
+++ binance_trading_bot/utilities.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_market_list(client):
     marketList = pd.DataFrame(client.get_products()['data'])
-#    marketList = marketList[(marketList['quoteAsset']=='BTC')]
+    marketList = marketList[(marketList['quoteAsset']=='BTC')]
     marketList = marketList[['symbol', 'tradedMoney']]
     return marketList
 
