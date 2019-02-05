64b6a3d0253d6c9cb6150378b8c1030f606083fa
trinhvv <vanvuong.trinh@gmail.com>
2019-02-05 05:27:58 +0100

    Commit time: 2019-02-05 05:27:58

--- binance_trading_bot/order.py
+++ binance_trading_bot/order.py
@@ -8,15 +8,20 @@ def get_coin_info(client, market):
     qtyUnit = float(coinInfo[2]['stepSize'])
     return priceUnit, qtyUnit
 
-def set_buy_order(client, market, buyPrice):
-    priceUnit, qtyUnit = get_coin_info(client, market=market)
-    btcBalance = float(client.get_asset_balance(asset='BTC')['free'])
-    buyQty = min(0.05*btcBalance, 0.0011)/buyPrice
-    buyPrice = ('%.10f' % float(int(np.floor(buyPrice/priceUnit))*priceUnit)).rstrip('0').rstrip('.')
-    buyQty = float(('%.10f' % float(int(np.floor(buyQty/qtyUnit))*qtyUnit)).rstrip('0').rstrip('.'))
-    client.order_limit_buy(symbol=market, quantity=buyQty, price=buyPrice)
-    return (time.ctime()+ ": Buy order set: "+market)
-   
+def cancel_sell_order(client, market):
+    orders = client.get_open_orders(symbol=market)
+    for order in reversed(orders):
+        if order['side']=='SELL':
+            client.cancel_order(symbol=market, orderId=order['orderId'])
+    return (time.ctime()+ ": Sell order unset: "+market)
+    
+def cancel_buy_order(client, market):
+    orders = client.get_open_orders(symbol=market)
+    for order in reversed(orders):
+        if order['side']=='BUY':
+            client.cancel_order(symbol=market, orderId=order['orderId'])
+    return (time.ctime()+ ": Buy order unset: "+market)
+
 def set_sell_order(client, market, profitMin=3, profitMax=13, numTransactions=10):
     priceUnit, qtyUnit = get_coin_info(client, market=market)
     coinBalance = float(client.get_asset_balance(asset=market[:-3])['free'])
@@ -37,25 +42,32 @@ def set_sell_order(client, market, profitMin=3, profitMax=13, numTransactions=10
     client.order_limit_sell(symbol=market, quantity=sellQty, price=sellPrice)
     return (time.ctime()+ ": Sell order set: "+market)
 
-def cancel_sell_order(client, market):
-    orders = client.get_open_orders(symbol=market)
-    for order in reversed(orders):
-        if order['side']=='SELL':
-            client.cancel_order(symbol=market, orderId=order['orderId'])
-    return (time.ctime()+ ": Sell order unset: "+market)
-    
-def cancel_buy_order(client, market):
-    orders = client.get_open_orders(symbol=market)
-    for order in reversed(orders):
-        if order['side']=='BUY':
-            client.cancel_order(symbol=market, orderId=order['orderId'])
-    return (time.ctime()+ ": Buy order unset: "+market)
+def buy_order(client, market, priceMin, priceMax, numTransactions):
+    cancel_buy_order(client, market)
+    priceUnit, qtyUnit = get_coin_info(client, market=market)
+    quoteBalance = float(client.get_asset_balance(asset=market[-3:])['free'])
+    priceList = np.arange(priceMin, priceMax, (priceMax-priceMin)/numTransactions)
+    buyQty = quoteBalance/numTransactions/priceMax/priceUnit
+    for buyPrice in priceList:
+        buyPrice = ('%.10f' % float(int(np.floor(buyPrice))*priceUnit)).rstrip('0').rstrip('.')
+        buyQty = float(('%.10f' % float(int(np.floor(buyQty/qtyUnit))*qtyUnit)).rstrip('0').rstrip('.'))
+        client.order_limit_buy(symbol=market, quantity=buyQty, price=buyPrice)
+    return (time.ctime()+ ": Buy order set: "+market)
 
-def account_balance(client):
-    balances = pd.DataFrame(client.get_account()['balances'])
-    balances = balances[(pd.to_numeric(balances['free'])+pd.to_numeric(balances['locked'])>=1e-6)]
-    balances['baseQty'] = balances['free']+balances['locked']
-    return balances
+def sell_order(client, market, priceMin, priceMax, numTransactions):
+    cancel_sell_order(client, market)
+    priceUnit, qtyUnit = get_coin_info(client, market=market)
+    assetBalance = float(client.get_asset_balance(asset=market[:-3])['free'])
+    priceList = np.arange(priceMin, priceMax, (priceMax-priceMin)/numTransactions)
+    sellQty = assetBalance/numTransactions
+    for sellPrice in priceList:
+        sellPrice = ('%.10f' % float(int(np.floor(sellPrice))*priceUnit)).rstrip('0').rstrip('.')
+        sellQty = float(('%.10f' % float(int(np.floor(sellQty/qtyUnit))*qtyUnit)).rstrip('0').rstrip('.'))
+        client.order_limit_sell(symbol=market, quantity=sellQty, price=sellPrice)
+    return (time.ctime()+ ": Sell order set: "+market)
+
+
+
 
 
 
